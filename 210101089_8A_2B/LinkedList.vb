bf658ec7c5446541645e77fc99f7a19a1dd33f28
Ridhiman <rdhkdh21@gmail.com>
2024-03-03 16:26:48 +0530

    vector+ll modified

--- 210101089_8A_2B/LinkedList.vb
+++ 210101089_8A_2B/LinkedList.vb
@@ -2,14 +2,11 @@
 
 Public Class LinkedList
     ' Global Variables
-    Dim n As Integer = 0 'size of linked list
-    Dim i As Integer = 0
+    Dim n As Integer = 0  'size of linked list
+    Dim i As Integer = 0  'index
+    Dim temp As Integer
 
-    Private Sub FormClosedHandler(ByVal sender As Object, ByVal e As FormClosedEventArgs)
-        ' Close the current form when the new form is closed
-        Me.Close()
-    End Sub
-
+    'wait function used in searching and traversing operations
     Private Sub wait(ByVal seconds As Integer)
         For i As Integer = 0 To seconds * 100
             System.Threading.Thread.Sleep(10)
@@ -17,176 +14,196 @@ Public Class LinkedList
         Next
     End Sub
 
+    'insert button
     Private Sub insert_btn_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles insert_btn.Click
-        Dim textBoxToUpdate As TextBox = DirectCast(LL_panel.Controls("Null_tb"), TextBox)
-        If textBoxToUpdate IsNot Nothing Then
-            LL_panel.Controls.Remove(textBoxToUpdate)
-        End If
-        n = n + 1
-
-        Dim textBox As New TextBox()
-        textBox.Name = "TextBox" & (n - 1).ToString() 'TextBox0, TextBox1...
-        textBox.Location = New Point(5 + 50 * (n - 1), 5) 'Adjust location as needed
-        textBox.Size = New Size(45, 45) 'Adjust size as needed
-        textBox.Text = insert_tb.Text
-        textBox.BackColor = Color.PaleTurquoise
-        textBox.ForeColor = Color.Black
-        textBox.ReadOnly = True
-
-        LL_panel.Controls.Add(textBox)
-
-        If n = 1 Then
-            Dim l1 As New Label()
-            l1.Name = "head"
-            l1.Location = New Point(5, 35) 'Adjust location as needed
-            l1.Size = New Size(45, 45) 'Adjust size as needed
-            l1.Text = "Head"
-            l1.ForeColor = Color.Black
-
-            LL_panel.Controls.Add(l1)
-        End If
-
-        Dim dummy_tb As New TextBox()
-        dummy_tb.Name = "Null_tb"
-        dummy_tb.Location = New Point(5 + 50 * (n), 5) 'Adjust location as needed
-        dummy_tb.Size = New Size(45, 45) 'Adjust size as needed
-        dummy_tb.Text = "NULL"
-        dummy_tb.BackColor = Color.PaleTurquoise
-        dummy_tb.ForeColor = Color.Black
-        dummy_tb.ReadOnly = True
-        LL_panel.Controls.Add(dummy_tb)
-    End Sub
-
-    Private Sub update_btn_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles update_btn.Click
-        Dim flag As Integer = 0
-        For i As Integer = 0 To n - 1
-            Dim str As String
-            str = "TextBox" & i.ToString() 'TextBoxi, for index i
-
-            ' Find the TextBox with a specific name
-            Dim textBoxToUpdate As TextBox = DirectCast(LL_panel.Controls(str), TextBox)
-            If textBoxToUpdate IsNot Nothing Then
-                textBoxToUpdate.BackColor = Color.Red
-
-                wait(1)    'sleep for 1 sec
-                textBoxToUpdate.BackColor = Color.PaleTurquoise
-
-                If textBoxToUpdate.Text = update_tb.Text Then
-                    textBoxToUpdate.BackColor = Color.Green
-                    textBoxToUpdate.Text = new_num_tb.Text
-                    flag = 1
-                    Exit For
-                End If
-            End If
-        Next
-
-        If flag = 0 Then   'If not found
-            update_tb.Text = "Not found"
-        End If
-    End Sub
-
-    Private Sub delete_btn_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles delete_btn.Click
-        Dim flag As Integer = 0
-        For i As Integer = 0 To n - 1
-            Dim str As String
-            str = "TextBox" & i.ToString() 'TextBoxi, for index i
-
-            ' Find the TextBox with a specific name
-            Dim textBoxToUpdate As TextBox = DirectCast(LL_panel.Controls(str), TextBox)
-            If textBoxToUpdate IsNot Nothing Then
-                textBoxToUpdate.BackColor = Color.Red
-
-                wait(1)    'sleep for 1 sec
-                textBoxToUpdate.BackColor = Color.PaleTurquoise
-
-
-                If textBoxToUpdate.Text = delete_tb.Text Then
+        'Check if input is integer and within range
+        If Integer.TryParse(insert_tb.Text, temp) Then
+            If temp <= 1000 And temp >= -1000 Then
+                'remove null pointer
+                Dim textBoxToUpdate As TextBox = DirectCast(LL_panel.Controls("Null_tb"), TextBox)
+                If textBoxToUpdate IsNot Nothing Then
                     LL_panel.Controls.Remove(textBoxToUpdate)
-                    flag = 1
-                    Exit For
                 End If
-            End If
-        Next
+                n = n + 1  'increment the size
 
-        If flag = 0 Then   'If not found
-            delete_tb.Text = "Not found"
+                'create new element at end of list
+                Dim textBox As New TextBox()
+                textBox.Name = "TextBox" & (n - 1).ToString() 'TextBox0, TextBox1...
+                textBox.Location = New Point(5 + 50 * (n - 1), 5) 'Adjust location as needed
+                textBox.Size = New Size(45, 45) 'Adjust size as needed
+                textBox.Text = insert_tb.Text
+                textBox.BackColor = Color.PaleTurquoise
+                textBox.ForeColor = Color.Black
+                textBox.ReadOnly = True
+
+                LL_panel.Controls.Add(textBox)
+
+                'add head pointer if its the beginning of linked list
+                If n = 1 Then
+                    Dim l1 As New Label()
+                    l1.Name = "head"
+                    l1.Location = New Point(5, 35) 'Adjust location as needed
+                    l1.Size = New Size(45, 45) 'Adjust size as needed
+                    l1.Text = "Head"
+                    l1.ForeColor = Color.Black
+
+                    LL_panel.Controls.Add(l1)
+                End If
+
+                'add null pointer at the end
+                Dim dummy_tb As New TextBox()
+                dummy_tb.Name = "Null_tb"
+                dummy_tb.Location = New Point(5 + 50 * (n), 5) 'Adjust location as needed
+                dummy_tb.Size = New Size(45, 45) 'Adjust size as needed
+                dummy_tb.Text = "NULL"
+                dummy_tb.BackColor = Color.PaleTurquoise
+                dummy_tb.ForeColor = Color.Black
+                dummy_tb.ReadOnly = True
+                LL_panel.Controls.Add(dummy_tb)
+            Else
+                insert_tb.Text = "Range error"
+            End If
+        Else
+            insert_tb.Text = "Invalid Input"
         End If
     End Sub
 
+    'update button
+    Private Sub update_btn_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles update_btn.Click
+        'Check if input is integer and within range
+        If Integer.TryParse(update_tb.Text, temp) Then
+            If temp <= 1000 And temp >= -1000 Then
+                'check if new input is integer and within range
+                If Integer.TryParse(new_num_tb.Text, temp) Then
+                    If temp <= 1000 And temp >= -1000 Then
+                        '-----------------------------------------------------------
+                        Dim flag As Integer = 0 'indicates 1=found/0=not found
+                        For i As Integer = 0 To n - 1
+                            Dim str As String
+                            str = "TextBox" & i.ToString() 'TextBoxi, for index i
+
+                            ' Find the TextBox with a specific name
+                            Dim textBoxToUpdate As TextBox = DirectCast(LL_panel.Controls(str), TextBox)
+                            If textBoxToUpdate IsNot Nothing Then
+                                textBoxToUpdate.BackColor = Color.Red
+
+                                wait(1)    'sleep for 1 sec
+                                textBoxToUpdate.BackColor = Color.PaleTurquoise
+
+                                If textBoxToUpdate.Text = update_tb.Text Then
+                                    textBoxToUpdate.BackColor = Color.Green
+                                    textBoxToUpdate.Text = new_num_tb.Text
+                                    flag = 1
+                                    Exit For
+                                End If
+                            End If
+                        Next
+
+                        If flag = 0 Then   'If not found
+                            update_tb.Text = "Not found"
+                        End If
+                        '------------------------------------------------------------------------
+                    Else
+                        new_num_tb.Text = "Range error"
+                    End If
+                Else
+                    new_num_tb.Text = "Invalid Input"
+                End If
+            Else
+                update_tb.Text = "Range error"
+            End If
+        Else
+            update_tb.Text = "Invalid Input"
+        End If
+    End Sub
+
+    'delete button
+    Private Sub delete_btn_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles delete_btn.Click
+        'Check if input is integer and within range
+        If Integer.TryParse(delete_tb.Text, temp) Then
+            If temp <= 1000 And temp >= -1000 Then
+                '---------------------------------------------
+                Dim flag As Integer = 0
+                For i As Integer = 0 To n - 1
+                    Dim str As String
+                    str = "TextBox" & i.ToString() 'TextBoxi, for index i
+
+                    ' Find the TextBox with a specific name
+                    Dim textBoxToUpdate As TextBox = DirectCast(LL_panel.Controls(str), TextBox)
+                    If textBoxToUpdate IsNot Nothing Then
+                        textBoxToUpdate.BackColor = Color.Red
+
+                        wait(1)    'sleep for 1 sec
+                        textBoxToUpdate.BackColor = Color.PaleTurquoise
+
+
+                        If textBoxToUpdate.Text = delete_tb.Text Then
+                            LL_panel.Controls.Remove(textBoxToUpdate)
+                            flag = 1
+                            Exit For
+                        End If
+                    End If
+                Next
+
+                If flag = 0 Then   'If not found
+                    delete_tb.Text = "Not found"
+                End If
+                '---------------------------------------------
+            Else
+                delete_tb.Text = "Range error"
+            End If
+        Else
+            delete_tb.Text = "Invalid Input"
+        End If
+    End Sub
+
+    'search button
     Private Sub search_btn_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles search_btn.Click
-        For i As Integer = 0 To n - 1
-            Dim str As String
-            str = "TextBox" & i.ToString() 'TextBoxi, for index i
+        'Check if input is integer and within range
+        If Integer.TryParse(search_tb.Text, temp) Then
+            If temp <= 1000 And temp >= -1000 Then
+                '---------------------------------------------
+                For i As Integer = 0 To n - 1
+                    Dim str As String
+                    str = "TextBox" & i.ToString() 'TextBoxi, for index i
 
-            ' Find the TextBox with a specific name
-            Dim textBoxToUpdate As TextBox = DirectCast(LL_panel.Controls(str), TextBox)
-            If textBoxToUpdate IsNot Nothing Then
-                textBoxToUpdate.BackColor = Color.Red
+                    ' Find the TextBox with a specific name
+                    Dim textBoxToUpdate As TextBox = DirectCast(LL_panel.Controls(str), TextBox)
+                    If textBoxToUpdate IsNot Nothing Then
+                        textBoxToUpdate.BackColor = Color.Red
 
-                wait(1)    'sleep for 1 sec
-                textBoxToUpdate.BackColor = Color.PaleTurquoise
+                        wait(1)    'sleep for 1 sec
+                        textBoxToUpdate.BackColor = Color.PaleTurquoise
 
-                If textBoxToUpdate.Text = search_tb.Text Then
-                    textBoxToUpdate.BackColor = Color.Green
-                    search_tb.Text = "Found"
-                    Exit For
+                        If textBoxToUpdate.Text = search_tb.Text Then
+                            textBoxToUpdate.BackColor = Color.Green
+                            search_tb.Text = "Found"
+                            Exit For
+                        End If
+                    End If
+                Next
+
+                If search_tb.Text <> "Found" Then   'If not found
+                    search_tb.Text = "Not found"
                 End If
+                '---------------------------------------------
+            Else
+                search_tb.Text = "Range error"
             End If
-        Next
-
-        If search_tb.Text <> "Found" Then   'If not found
-            search_tb.Text = "Not found"
+        Else
+            search_tb.Text = "Invalid Input"
         End If
     End Sub
 
-    Private Sub reverse_btn_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
-        For i As Integer = 0 To n - 1
-            Dim str As String
-            str = "TextBox" & i.ToString() 'TextBoxi, for index i
-
-            ' Find the TextBox with a specific name
-            Dim textBoxToUpdate As TextBox = DirectCast(LL_panel.Controls(str), TextBox)
-            If textBoxToUpdate IsNot Nothing Then
-                textBoxToUpdate.BackColor = Color.Red
-
-                wait(1)    'sleep for 1 sec
-                textBoxToUpdate.BackColor = Color.PaleTurquoise
-
-                If textBoxToUpdate.Text = search_tb.Text Then
-                    textBoxToUpdate.BackColor = Color.Green
-                    search_tb.Text = "Found"
-                    Exit For
-                End If
-            End If
-        Next
-
-        If search_tb.Text <> "Found" Then   'If not found
-            search_tb.Text = "Not found"
-        End If
-    End Sub
-
+    'reset button
     Private Sub reset_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles reset.Click
+        n = 0
+        i = 0
         LL_panel.Controls.Clear()
         insert_tb.Clear()
         search_tb.Clear()
         update_tb.Clear()
         delete_tb.Clear()
         new_num_tb.Clear()
-    End Sub
-
-    Private Sub quiz_btn_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles quiz_btn.Click
-        Dim f2 As New Quiz()
-        AddHandler f2.FormClosed, AddressOf FormClosedHandler
-        ' Show the BubbleSort form
-        f2.Show()
-        Me.Hide()
-    End Sub
-
-    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
-        Dim f2 As New Home()
-        AddHandler f2.FormClosed, AddressOf FormClosedHandler
-        ' Show the BubbleSort form
-        f2.Show()
-        Me.Hide()
     End Sub
 End Class
--- 210101089_8A_2B/Vector.vb
+++ 210101089_8A_2B/Vector.vb
@@ -2,29 +2,39 @@
 
 Public Class Vector
     ' Global Variables
-    Dim n As Integer = 0
-    Dim i As Integer = 0
-    Private Sub FormClosedHandler(ByVal sender As Object, ByVal e As FormClosedEventArgs)
-        ' Close the current form when the new form is closed
-        Me.Close()
-    End Sub
+    Dim n As Integer = 0   'size
+    Dim i As Integer = 0   'index
+    Dim temp As Integer
 
+    'Insert button function
     Private Sub Insert_elem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Insert_elem.Click
-        n = n + 1
-        Dim textBox As New TextBox()
-        textBox.Name = "TextBox" & (n - 1).ToString()
-        textBox.Location = New Point(5 + 50 * (n - 1), 5) 'Adjust location as needed
-        textBox.Size = New Size(45, 45) 'Adjust size as needed
-        textBox.Text = insert_textbox.Text
-        textBox.BackColor = Color.PaleTurquoise
-        textBox.ForeColor = Color.Black
-        textBox.ReadOnly = True
+        'Check whether input enetered is numerical or not
+        If Integer.TryParse(insert_textbox.Text, temp) Then
+            'Check if number is within range
+            If temp <= 1000 And temp >= -1000 Then
+                n = n + 1   'update size of vector
+                Dim textBox As New TextBox()   'add new element named TextBox_i
+                textBox.Name = "TextBox" & (n - 1).ToString()
+                textBox.Location = New Point(5 + 50 * (n - 1), 5) 'Adjust location as needed
+                textBox.Size = New Size(45, 45) 'Adjust size as needed
+                textBox.Text = insert_textbox.Text
+                textBox.BackColor = Color.PaleTurquoise
+                textBox.ForeColor = Color.Black
+                textBox.ReadOnly = True
 
-        Panel1.Controls.Add(textBox)
-        Size_display.Text = n
+                Panel1.Controls.Add(textBox)  'Add textbox to panel
+                Size_display.Text = n  'update size display
+            Else
+                insert_textbox.Text = "Range error"
+            End If
+        Else
+            insert_textbox.Text = "Invalid Input"
+        End If
     End Sub
 
+    'Access index button 
     Private Sub Access_index_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Access_index.Click
+        'Check whether index is an integer and within range
         If Integer.TryParse(access_textbox.Text, i) Then
             If i < n And i >= 0 Then
                 ' Find the TextBox with a specific name
@@ -44,7 +54,10 @@ Public Class Vector
         End If
     End Sub
 
+    'Clears all input/output
     Private Sub reset_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles reset.Click
+        n = 0
+        i = 0
         Panel1.Controls.Clear()
         insert_textbox.Clear()
         access_textbox.Clear()
@@ -55,62 +68,57 @@ Public Class Vector
         access_display.Clear()
     End Sub
 
+    'Update an element in vector
     Private Sub UpdateButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles UpdateButton.Click
+        'Check whether index is an integer and within range
         If Integer.TryParse(update_textbox.Text, i) Then
             If i < n And i >= 0 Then
-                Dim str As String
-                str = "TextBox" & update_textbox.Text 'TextBoxi, for index i
+                'Check whether new number is an integer and within range
+                If Integer.TryParse(new_num_txbx.Text, temp) Then
+                    If temp <= 1000 And temp >= -1000 Then
+                        ' Find the TextBox with a specific name
+                        Dim str As String
+                        str = "TextBox" & update_textbox.Text 'TextBoxi, for index i
+                        Dim textBoxToUpdate As TextBox = DirectCast(Panel1.Controls(str), TextBox)
 
-                ' Find the TextBox with a specific name
-                Dim textBoxToUpdate As TextBox = DirectCast(Panel1.Controls(str), TextBox)
-
-                ' Update the content of the TextBox
-                If textBoxToUpdate IsNot Nothing Then
-                    textBoxToUpdate.Text = new_num_txbx.Text
-                    textBoxToUpdate.ForeColor = Color.Red
+                        ' Update the content of the TextBox
+                        If textBoxToUpdate IsNot Nothing Then
+                            textBoxToUpdate.Text = new_num_txbx.Text
+                            textBoxToUpdate.ForeColor = Color.Red
+                        End If
+                    Else
+                        new_num_txbx.Text = "Range error"  'new number outside range
+                    End If
+                Else
+                    new_num_txbx.Text = "Invalid Input"   'new number is not integer
                 End If
             Else
-                update_textbox.Text = "Out of range."
+                update_textbox.Text = "Range error"   'index out of range
             End If
         Else
-            update_textbox.Text = "Invalid Input."
+            update_textbox.Text = "Invalid Input."    'index not integer
         End If
     End Sub
 
+    'Delete element from vector
     Private Sub Remove_elem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Remove_elem.Click
+        'Check whether index is an integer and within range
         If Integer.TryParse(remove_textbox.Text, i) Then
             If i < n And i >= 0 Then
+                ' Find the TextBox with a specific name
                 Dim str As String
                 str = "TextBox" & remove_textbox.Text 'TextBoxi, for index i
-
-                ' Find the TextBox with a specific name
                 Dim textBoxToUpdate As TextBox = DirectCast(Panel1.Controls(str), TextBox)
 
-                ' Update the content of the TextBox
+                ' Remove the TextBox
                 If textBoxToUpdate IsNot Nothing Then
                     Panel1.Controls.Remove(textBoxToUpdate)
                 End If
             Else
-                remove_textbox.Text = "Out of range."
+                remove_textbox.Text = "Range error"  'index out of range
             End If
         Else
-            remove_textbox.Text = "Invalid Input."
+            remove_textbox.Text = "Invalid Input."   'index not an integer
         End If
-    End Sub
-
-    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
-        Dim f2 As New Quiz()
-        AddHandler f2.FormClosed, AddressOf FormClosedHandler
-        ' Show the BubbleSort form
-        f2.Show()
-        Me.Hide()
-    End Sub
-
-    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
-        Dim f2 As New Home()
-        AddHandler f2.FormClosed, AddressOf FormClosedHandler
-        ' Show the BubbleSort form
-        f2.Show()
-        Me.Hide()
     End Sub
 End Class
